8967529fcbe1bdbcb53f02431a7cb5a7180cba22
Anjali MahawR <anjalimahawar454@gmail.com>
2022-01-28 01:34:56 -0800

    28th jan

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import Login from './page/Login';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
